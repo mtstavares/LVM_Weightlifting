06e832a707b03787813cb9d17832f4c040a8bbea
MATHEUS RODRIGUES CRUZ TAVARES <46047228895@cmdo.policiamilitar.sp.gov.br>
2026-07-03 16:10:49 -0300

    Fix storage auth module dependency

--- apps/api/src/shared/infrastructure/storage/storage.module.ts
+++ apps/api/src/shared/infrastructure/storage/storage.module.ts
@@ -1,10 +1,12 @@
 import { Global, Module } from '@nestjs/common';
+import { AuthModule } from '../../../modules/auth/auth.module';
 import { FILE_STORAGE_SERVICE } from './storage.token';
 import { LocalFileStorageService } from './local-file-storage.service';
 import { PrivateMediaController } from './private-media.controller';
 
 @Global()
 @Module({
+  imports: [AuthModule],
   controllers: [PrivateMediaController],
   providers: [{ provide: FILE_STORAGE_SERVICE, useClass: LocalFileStorageService }],
   exports: [FILE_STORAGE_SERVICE]
